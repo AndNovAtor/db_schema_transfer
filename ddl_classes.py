3306d6640a3acd7143f727520763a25e0d94bfee
Andrey <andnovator@gmail.com>
2015-10-19 00:00:28 +0300

    Domain and tables parsing
    - Edit ddl classes:
    - fix first initializations of classes properties - move into __init__
    (was as class properties in old version)
    - make only one __init__ (Python not support method overwrite), old
    version of __init__ - commented
    - make __init__ without parametrs (was as specific initialization, by
    specific task - it is incorrect)
    - fix class type check by type() - .__name__ also required
    - fix some other bugs in classes methods
    - Edit main program
    - parsing domains and tables without indexes and constraint now work (i
    hope, correctly :grin:)
    - place all ddl objects properties in parsing functions
    - change calling parsing functions (and its parameters)
    - add blank of constraint parsing function
    - add sqlite (sqlite3) using (create temp sqlite db) with sqlite3 module

--- ddl_classes.py
+++ ddl_classes.py
@@ -1,34 +1,34 @@
 class Domain:
-    name = ""
-    description = ""
-    type = ""
-    align = ""
-    width = 0
-    precision = ""
-    props = ""
-    char_length = 0
-    # attrs = {}
+    def __init__(self):
+        self.name = ""
+        self.description = ""
+        self.type = ""
+        self.align = ""
+        self.width = 0
+        self.precision = ""
+        self.props = ""
+        self.char_length = 0
 
-    def __init__(self, attr_dict):
-        self.name = attr_dict.get("name", "")
-        # TODO Использование .pop() изменит объект снаружи, однако это уменшит поиск, в теории. Нужно ли заменить?
-        self.description = attr_dict.get("description", "")
-        self.type = attr_dict.get("type", "")
-        self.align = attr_dict.get("align", "")
-        self.width = attr_dict.get("width", 0)
-        self.precision = attr_dict.get("precision", 0)
-        self.props = attr_dict.get("props", "")
-        self.char_length = attr_dict.get("align", 0)
+    # def __init__(self, attr_dict):
+    #     self.name = attr_dict.get("name", "")
+    #     # TODO Использование .pop() изменит объект снаружи, однако это уменшит поиск, в теории. Нужно ли заменить?
+    #     self.description = attr_dict.get("description", "")
+    #     self.type = attr_dict.get("type", "")
+    #     self.align = attr_dict.get("align", "")
+    #     self.width = int(attr_dict.get("width", 0))
+    #     self.precision = attr_dict.get("precision", 0)
+    #     self.props = attr_dict.get("props", "")
+    #     self.char_length = int(attr_dict.get("char_length", 0))
 
-    def __init__(self, name_i, type_i, align_i, width_i, descr_i="", precision_i="", props_i="", char_length_i=0):
-        self.name = name_i
-        self.description = descr_i
-        self.type = type_i
-        self.align = align_i
-        self.width = width_i
-        self.precision = precision_i
-        self.props = props_i
-        self.char_length = char_length_i
+    # def __init__(self, name_i, type_i, align_i, width_i, descr_i="", precision_i="", props_i="", char_length_i=0):
+    #     self.name = name_i
+    #     self.description = descr_i
+    #     self.type = type_i
+    #     self.align = align_i
+    #     self.width = width_i
+    #     self.precision = precision_i
+    #     self.props = props_i
+    #     self.char_length = char_length_i
 
     # def get_val(self, attr_name, default=None):
     #     return {
@@ -43,116 +43,173 @@ class Domain:
 
 
 class Table:
-    name = ""
-    description = ""
-    props = ""
-    ht_table_flags = ""
-    fields = []
-    pr_constraint = None
-    fr_constraints = []
-    # attrs = {}
+    def __init__(self):
+        self.name = ""
+        self.description = ""
+        self.props = ""
+        self.ht_table_flags = ""
+        self.fields = []
+        self.pr_constraint = None
+        self.fr_constraints = []
 
-    def __init__(self, attr_dict):
-        self.name = attr_dict.get("name", "")
-        self.description = attr_dict.get("description", "")
-        self.props = attr_dict.get("props", "")
-        self.ht_table_flags = attr_dict.get("ht_table_flags", "")
+    # def __init__(self, attr_dict):
+    #     self.name = attr_dict.get("name", "")
+    #     self.description = attr_dict.get("description", "")
+    #     self.props = attr_dict.get("props", "")
+    #     self.ht_table_flags = attr_dict.get("ht_table_flags", "")
+    #     self.fields = []
+    #     self.pr_constraint = None
+    #     self.fr_constraints = []
 
-    def __init__(self, attr_dict, fields_lst, prim_constraint, for_constraints):
-        self.name = attr_dict.get("name", "")
-        self.description = attr_dict.get("description", "")
-        self.props = attr_dict.get("props", "")
-        self.ht_table_flags = attr_dict.get("ht_table_flags", "")
-        self.fields = fields_lst
-        self.pr_constraint = prim_constraint
-        self.fr_constraints = for_constraints
+    # # TODO Нужно подумать, нужно ли где-то копирование
+    # def __init__(self, name_i, fields_lst, prim_constraint, for_constraints, descr_i="", props_i="", ht_table_flags=""):
+    #     self.name = name_i
+    #     self.description = descr_i
+    #     self.props = props_i
+    #     self.ht_table_flags = ht_table_flags
+    #     self.fields = fields_lst
+    #     self.pr_constraint = prim_constraint
+    #     self.fr_constraints = for_constraints
 
+    # def __init__(self, attr_dict, fields_lst, prim_constraint, for_constraints):
+    #     self.name = attr_dict.get("name", "")
+    #     self.description = attr_dict.get("description", "")
+    #     self.props = attr_dict.get("props", "")
+    #     self.ht_table_flags = attr_dict.get("ht_table_flags", "")
+    #     self.fields = fields_lst
+    #     self.pr_constraint = prim_constraint
+    #     self.fr_constraints = for_constraints
 
-# TODO Нужно подумать, нужно ли где-то копирование
-    def __init__(self, name_i, description_i, props_i, ht_table_flags_i, fields_i, pr_constraint_i, fr_constraints_i):
-        self.name = name_i
-        self.description = description_i
-        self.props = props_i
-        self.ht_table_flags = ht_table_flags_i
-        self.fields = pr_constraint_i
-        self.pr_constraint = pr_constraint_i
-        self.fr_constraints = fr_constraints_i
+    def append_field(self, field):
+        if type(field).__name__ == "Field":
+            self.fields.append(field)
 
 
 class Field:
-    name = ""
-    rname = ""
-    domain_name = ""
-    domain = None
-    description = ""
-    props = ""
+    def __init__(self):
+        self.name = ""
+        self.rname = ""
+        self.description = ""
+        self.domain_name = ""
+        self.domain = None
+        self.props = ""
 
-    def __init__(self, attr_dict):
-        self.name = attr_dict.get("name", "")
-        self.rname = attr_dict.get("rname", "")
-        self.domain_name = attr_dict.get("domain", "")
-        self.description = attr_dict.get("description", "")
-        self.props = attr_dict.get("props", "")
+    def init_domain(self, domains):
+        if type(domains).__name__ == "list":
+            for domain_item in domains:
+                if type(domain_item).__name__ == "Domains":
+                    if domain_item.name == self.domain_name:
+                        self.domain = domain_item
+                        break
 
-    def __init__(self, attr_dict, domain_list):
-        self.name = attr_dict.get("name", "")
-        self.rname = attr_dict.get("rname", "")
-        self.domain_name = attr_dict.get("domain", "")
-        self.description = attr_dict.get("description", "")
-        self.props = attr_dict.get("props", "")
-        # domain = "{}"d for d in domain_list if d.name==self.domain_name]
-        for node in domain_list:
-            if node.name == self.domain_name:
-                self.domain = node
-                break
+    # def __init__(self, name_i, rname_i, domain_name_i, descr_i="", props_i=""):
+    #     self.name = name_i
+    #     self.rname = rname_i
+    #     self.description = descr_i
+    #     self.domain_name = domain_name_i
+    #     self.domain = None
+    #     self.props = props_i
+
+    # def __init__(self, attr_dict):
+    #     self.name = attr_dict.get("name", "")
+    #     self.rname = attr_dict.get("rname", "")
+    #     self.domain_name = attr_dict.get("domain", "")
+    #     self.description = attr_dict.get("description", "")
+    #     self.props = attr_dict.get("props", "")
+
+    # def __init__(self, attr_dict, domain_list):
+    #     self.name = attr_dict.get("name", "")
+    #     self.rname = attr_dict.get("rname", "")
+    #     self.domain_name = attr_dict.get("domain", "")
+    #     self.description = attr_dict.get("description", "")
+    #     self.props = attr_dict.get("props", "")
+    #     # domain = "{}"d for d in domain_list if d.name==self.domain_name]
+    #     for node in domain_list:
+    #         if node.name == self.domain_name:
+    #             self.domain = node
+    #             break
 
 
 class Constraint:
-    item_name = ""
-    item = None
 
-    def init_item(self, item_i):
-        if item_i is None:
-            self.item = None
-        else:
-            if type(item_i) == "string":
+    def _init_item(self, item_i):
+        if item_i is not None:
+            if type(item_i).__name__ == "string":
                 self.item_name = item_i
+                self.item = None
             else:
-                self.item = item_i
-                self.item_name = item_i.name
+                if type(item_i).__name__ == "Field":
+                    self.item_name = item_i.name
+                    self.item = item_i
 
 
 class PrConstraint (Constraint):
+    def __init__(self):
+        self.item_name = ""
+        self.item = None
+
     def __init__(self, item_i):
-        self.init_item(item_i)
-        self.item_name = item_i.name
+        self._init_item(item_i)
+
+    # def __init__(self, attr_dict):
+    #     self.item_name = attr_dict.get("items", "")
+    #     self.item = None
 
 
 class ForConstraint (Constraint):
-    reference = None
-    ref_name = ""
-    props = ""
+    def __init__(self):
+        self.item_name = ""
+        self.item = None
+        self.ref_name = ""
+        self.reference = None
+        self.props = ""
 
-    def __init__(self, item_i, refer_item, props_i):
-        self.init_item(item_i)
-        self.item_name = item_i.name
-        self.reference = refer_item
+    def _init_ref(self, ref_item):
+        if ref_item is None:
+            self.ref_name = ""
+            self.ref_item = None
+        else:
+            if type(ref_item).__name__ == "string":
+                self.ref_name = ref_item
+                self.reference = None
+            else:
+                self.ref_name = ref_item.name
+                self.reference = ref_item
+
+    def __init__(self, item_i, refer_item, props_i=""):
+        self._init_item(item_i)
+        self._init_ref(refer_item)
         self.props = props_i
+
+    # def __init__(self, attr_dict):
+    #     self.item_name = attr_dict.get("items", "")
+    #     self.item = None
+    #     self.ref_name = attr_dict.get("reference", "")
+    #     self.reference = None
 
 
 class Index:
-    field_name = ""
-    field = None
-    props = ""
+    def __init__(self):
+        self.field_name = ""
+        self.field = None
+        self.props = ""
 
-    def __init__(self, field_i, props_i=""):
-        if field_i is None:
-            self.field = None
-        else:
-            if type(field_i) == "string":
-                self.field_name = field_i
+    def __init__(self, field_varr, props_i=""):
+        if field_varr is not None:
+            if type(field_varr).__name__ == "string":
+                self.field_name = field_varr
+                self.field = None
             else:
-                self.field = field_i
-                self.field_name = field_i.name
+                if type(field_varr).__name__ == "Field":
+                    self.field_name = field_varr.name
+                    self.field = field_varr
+                else:
+                    self.field_name = ""
+                    self.field = None
         self.props = props_i
+
+    # def __init__(self, attr_dict):
+    #     self.field_name = attr_dict.get("field", "")
+    #     self.props = attr_dict.get("props", "")
+
 
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ __author__ = 'NovAtor'
 # import xml.dom.minidom
 # import xml.dom
 import xml.etree.ElementTree as ET
+import sqlite3
 import xml.etree
 # from platform import system
 
@@ -93,20 +94,107 @@ print_xml_tree(xmlRoot)
 domain_lst = []
 
 
-def parse_domains(root):
+def parse_domains(root, domains):
     dom_num = 0
-    domains = []
-    for domain in root.find("domains"):
+    # domains = []
+    for domain_dict in root.find("domains"):
         dom_num += 1
         # print(" attr-s:", ["{}={}, ".format(k, v) for k, v in domain.attrib.items()])
-        domains.append(ddl_classes.Domain(domain.attrib))
-    return domains
+        domain = ddl_classes.Domain()
+
+        domain.name = domain_dict.get("name", "")
+        # TODO Использование .pop() изменит объект снаружи, однако это уменшит поиск, в теории. Нужно ли заменить?
+        domain.description = domain_dict.get("description", "")
+        domain.type = domain_dict.get("type", "")
+        domain.align = domain_dict.get("align", "")
+        domain.width = int(domain_dict.get("width", 0))
+        domain.precision = domain_dict.get("precision", 0)
+        domain.props = domain_dict.get("props", "")
+        domain.char_length = int(domain_dict.get("char_length", 0))
+
+        domains.append(domain)
+    return dom_num
 
 
-def parse_tables(root):
-    tables_num = 0
+def parse_tables(root, in_tables, domains):
+    tabl_num = 0
+    for table in root.find("tables"):
+        tabl_num += 1
+        table_obj = ddl_classes.Table()
 
-domain_lst = parse_domains(xmlRoot)
-print(xmlRoot[0].tag, xmlRoot[0].attrib, xmlRoot[1].text.strip(), '3')
-print(xmlRoot[1][0].tag, xmlRoot[1][0].attrib, xmlRoot[1][0].text, '3')
-print(domain_lst)
+        table_obj.name = table.get("name", "")
+        table_obj.description = table.get("description", "")
+        table_obj.props = table.get("props", "")
+        table_obj.ht_table_flags = table.get("ht_table_flags", "")
+        table_obj.fields = []
+        table_obj.pr_constraint = None
+        table_obj.fr_constraints = []
+
+        for field_dict in table:
+            field = ddl_classes.Field()
+
+            field.name = field_dict.get("name", "")
+            field.rname = field_dict.get("rname", "")
+            field.domain_name = field_dict.get("domain", "")
+            field.description = field_dict.get("description", "")
+            field.props = field_dict.get("props", "")
+            # TODO: domain = "{}"d for d in domain_list if d.name==self.domain_name]
+            for node in domains:
+                if node.name == field.domain_name:
+                    field.domain = node
+                    break
+
+            table_obj.append_field(field)
+        in_tables.append(table_obj)
+    return tabl_num
+
+
+# def parse_tables_fields(root, table, domains): #, tables_num):
+#     iteration = 0
+#     for table in root.find("tables"):
+#         for field in table.find("field"):
+#             table.append_field(ddl_classes.Field(field.attr, domains))
+#         iteration += 1
+
+def parse_tables_other(root, in_tables):
+    for table in root.find("tables"):
+        for constraint in table.find("constraint"):
+            if constraint.get("kind", "") == "PRIMARY":
+                la = {}
+            else:
+                if constraint.get("kind", "") == "FOREIGN":
+                    lald = {}
+                else:
+                    dldld = {}
+
+domain_lst = []
+domains_num = parse_domains(xmlRoot, domain_lst)
+tables = []
+tables_num = parse_tables(xmlRoot, tables, domain_lst)
+lala = []
+# parse_tables_other(xmlRoot, tables)
+# print(xmlRoot[0].tag, xmlRoot[0].attrib, xmlRoot[1].text.strip(), '3')
+# print(xmlRoot[1][0].tag, xmlRoot[1][0].attrib, xmlRoot[1][0].text, '3')
+# print(domain_lst)
+con = sqlite3.connect("temp.db")
+cur = con.cursor()
+cur.executescript("""
+    create table person(
+        firstname,
+        lastname,
+        age
+    );
+
+    create table book(
+        title,
+        author,
+        published
+    );
+
+    insert into book(title, author, published)
+    values (
+        'Dirk Gently''s Holistic Detective Agency',
+        'Douglas Adams',
+        1987
+    );
+    """)
