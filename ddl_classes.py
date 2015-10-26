a1b29bb1f57285045030705b020bc0c990a44919
Andrey <andnovator@gmail.com>
2015-10-26 04:19:39 +0300

    True parsing of domains, tables and fields
    - Edit ddl __init__ in classes - fix it and keep one __init__ in class
    - add to __init__ futured init proprties (not only __init_())
    - delete old import
    - delete old and incorrect code that is not use
    - fix init ddl objects in main.py: use .get(...) because there are
    etree.Element's objects ass ddl objects
    - add truse parsing of domains, tables and fields (with many sql code);
    use "inner join" there

--- ddl_classes.py
+++ ddl_classes.py
@@ -1,98 +1,51 @@
 class Domain:
-    def __init__(self):
-        self.name = ""
-        self.description = ""
-        self.type = ""
-        self.align = ""
-        self.width = 0
-        self.precision = ""
-        self.props = ""
-        self.char_length = 0
-
-    # def __init__(self, attr_dict):
-    #     self.name = attr_dict.get("name", "")
-    #     # TODO Использование .pop() изменит объект снаружи, однако это уменшит поиск, в теории. Нужно ли заменить?
-    #     self.description = attr_dict.get("description", "")
-    #     self.type = attr_dict.get("type", "")
-    #     self.align = attr_dict.get("align", "")
-    #     self.width = int(attr_dict.get("width", 0))
-    #     self.precision = attr_dict.get("precision", 0)
-    #     self.props = attr_dict.get("props", "")
-    #     self.char_length = int(attr_dict.get("char_length", 0))
-
-    # def __init__(self, name_i, type_i, align_i, width_i, descr_i="", precision_i="", props_i="", char_length_i=0):
-    #     self.name = name_i
-    #     self.description = descr_i
-    #     self.type = type_i
-    #     self.align = align_i
-    #     self.width = width_i
-    #     self.precision = precision_i
-    #     self.props = props_i
-    #     self.char_length = char_length_i
-
-    # def get_val(self, attr_name, default=None):
-    #     return {
-    #         "name": self.name,
-    #         "description": self.description,
-    #         "type": self.type,
-    #         "align": self.align,
-    #         "width": self.width,
-    #         "props": self.props,
-    #         "char_length": self.char_length,
-    #     }.get(attr_name.lower(), self.attrs.get(attr_name, default))
+    # TODO Нужно подумать, нужно ли где-то копирование
+    def __init__(self, name_i=None, descr_i=None, type_i=None, align_i=None, precision_i=None, props_i="", width_i=None,
+                 char_length_i=None, length_i=None, scale_i=None):
+        self.name = name_i
+        self.description = descr_i
+        self.type = type_i
+        self.align = align_i
+        self.width = width_i
+        self.precision = precision_i
+        self.props = props_i
+        self.char_length = char_length_i
+        self.length = length_i
+        self.scale = scale_i
 
 
 class Table:
-    def __init__(self):
-        self.name = ""
-        self.description = ""
-        self.props = ""
-        self.ht_table_flags = ""
-        self.fields = []
-        self.pr_constraint = None
-        self.fr_constraints = []
-
-    # def __init__(self, attr_dict):
-    #     self.name = attr_dict.get("name", "")
-    #     self.description = attr_dict.get("description", "")
-    #     self.props = attr_dict.get("props", "")
-    #     self.ht_table_flags = attr_dict.get("ht_table_flags", "")
-    #     self.fields = []
-    #     self.pr_constraint = None
-    #     self.fr_constraints = []
-
-    # # TODO Нужно подумать, нужно ли где-то копирование
-    # def __init__(self, name_i, fields_lst, prim_constraint, for_constraints, descr_i="", props_i="", ht_table_flags=""):
-    #     self.name = name_i
-    #     self.description = descr_i
-    #     self.props = props_i
-    #     self.ht_table_flags = ht_table_flags
-    #     self.fields = fields_lst
-    #     self.pr_constraint = prim_constraint
-    #     self.fr_constraints = for_constraints
-
-    # def __init__(self, attr_dict, fields_lst, prim_constraint, for_constraints):
-    #     self.name = attr_dict.get("name", "")
-    #     self.description = attr_dict.get("description", "")
-    #     self.props = attr_dict.get("props", "")
-    #     self.ht_table_flags = attr_dict.get("ht_table_flags", "")
-    #     self.fields = fields_lst
-    #     self.pr_constraint = prim_constraint
-    #     self.fr_constraints = for_constraints
+    def __init__(self, name_i=None, descr_i=None, props_i="", fields_lst=None, ht_table_flags="", prim_constraint=None,
+                 for_constraints=None, ind_lst=None):
+        self.name = name_i
+        self.description = descr_i
+        self.props = props_i
+        self.ht_table_flags = ht_table_flags
+        if fields_lst.__class__.__name__ == "list":
+            self.fields = {fld.name: fld for fld in fields_lst if fields_lst.__class__.__name__ == "Field"}
+            # self.fields_name_lst = [fld.name for fld in fields_lst if fields_lst.__class__.__name__ == "Field"]
+        else:
+            self.fields = {}
+            # self.fields_name_lst = []
+        self.pr_constraint = prim_constraint
+        self.fr_constraints = for_constraints if (for_constraints.__class__.__name__ == "list") else []
+        self.indexes = ind_lst if (ind_lst.__class__.__name__ == "list") else []
 
     def append_field(self, field):
         if type(field).__name__ == "Field":
-            self.fields.append(field)
+            if (field.name is not None) and (field.name is not ""):
+                self.fields[field.name] = field  # Add field with name as adding item to a dict
+                # self.fields_name_lst.append(field.name)
 
 
 class Field:
-    def __init__(self):
-        self.name = ""
-        self.rname = ""
-        self.description = ""
-        self.domain_name = ""
+    def __init__(self, name_i=None, rname_i=None, descr_i=None, domain_name_i=None, props_i=""):
+        self.name = name_i
+        self.rname = rname_i
+        self.description = descr_i
+        self.domain_name = domain_name_i
         self.domain = None
-        self.props = ""
+        self.props = props_i
 
     def init_domain(self, domains):
         if type(domains).__name__ == "list":
@@ -102,114 +55,56 @@ class Field:
                         self.domain = domain_item
                         break
 
-    # def __init__(self, name_i, rname_i, domain_name_i, descr_i="", props_i=""):
-    #     self.name = name_i
-    #     self.rname = rname_i
-    #     self.description = descr_i
-    #     self.domain_name = domain_name_i
-    #     self.domain = None
-    #     self.props = props_i
-
-    # def __init__(self, attr_dict):
-    #     self.name = attr_dict.get("name", "")
-    #     self.rname = attr_dict.get("rname", "")
-    #     self.domain_name = attr_dict.get("domain", "")
-    #     self.description = attr_dict.get("description", "")
-    #     self.props = attr_dict.get("props", "")
-
-    # def __init__(self, attr_dict, domain_list):
-    #     self.name = attr_dict.get("name", "")
-    #     self.rname = attr_dict.get("rname", "")
-    #     self.domain_name = attr_dict.get("domain", "")
-    #     self.description = attr_dict.get("description", "")
-    #     self.props = attr_dict.get("props", "")
-    #     # domain = "{}"d for d in domain_list if d.name==self.domain_name]
-    #     for node in domain_list:
-    #         if node.name == self.domain_name:
-    #             self.domain = node
-    #             break
-
 
 class Constraint:
-
     def _init_item(self, item_i):
-        if item_i is not None:
-            if type(item_i).__name__ == "string":
-                self.item_name = item_i
-                self.item = None
+        if type(item_i).__name__ == "str":
+            self.item_name = item_i
+            self.item = None
+        else:
+            if type(item_i).__name__ == "Field":
+                self.item_name = item_i.name
+                self.item = item_i
             else:
-                if type(item_i).__name__ == "Field":
-                    self.item_name = item_i.name
-                    self.item = item_i
+                self.item_name = None
+                self.item = None
 
 
-class PrConstraint (Constraint):
-    def __init__(self):
-        self.item_name = ""
-        self.item = None
-
-    def __init__(self, item_i):
+class PrConstraint(Constraint):
+    def __init__(self, item_i=None):
         self._init_item(item_i)
 
-    # def __init__(self, attr_dict):
-    #     self.item_name = attr_dict.get("items", "")
-    #     self.item = None
 
-
-class ForConstraint (Constraint):
-    def __init__(self):
-        self.item_name = ""
-        self.item = None
-        self.ref_name = ""
-        self.reference = None
-        self.props = ""
-
+class ForConstraint(Constraint):
     def _init_ref(self, ref_item):
-        if ref_item is None:
-            self.ref_name = ""
-            self.ref_item = None
+        if type(ref_item).__name__ == "str":
+            self.ref_name = ref_item
+            self.reference = None
         else:
-            if type(ref_item).__name__ == "string":
-                self.ref_name = ref_item
-                self.reference = None
-            else:
+            if type(ref_item).__name__ == "Table":
                 self.ref_name = ref_item.name
                 self.reference = ref_item
+            else:
+                self.ref_name = None
+                self.ref_item = None
 
-    def __init__(self, item_i, refer_item, props_i=""):
+    def __init__(self, item_i=None, refer_item=None, props_i=""):
         self._init_item(item_i)
         self._init_ref(refer_item)
         self.props = props_i
 
-    # def __init__(self, attr_dict):
-    #     self.item_name = attr_dict.get("items", "")
-    #     self.item = None
-    #     self.ref_name = attr_dict.get("reference", "")
-    #     self.reference = None
-
 
 class Index:
-    def __init__(self):
-        self.field_name = ""
-        self.field = None
-        self.props = ""
-
-    def __init__(self, field_varr, props_i=""):
-        if field_varr is not None:
-            if type(field_varr).__name__ == "string":
-                self.field_name = field_varr
-                self.field = None
+    def __init__(self, field_varr=None, props_i=""):
+        clazz = type(field_varr).__name__
+        if clazz == "str":
+            self.field_name = field_varr
+            self.field = None
+        else:
+            if clazz == "Field":
+                self.field_name = field_varr.name
+                self.field = field_varr
             else:
-                if type(field_varr).__name__ == "Field":
-                    self.field_name = field_varr.name
-                    self.field = field_varr
-                else:
-                    self.field_name = ""
-                    self.field = None
+                self.field_name = None
+                self.field = None
         self.props = props_i
-
-    # def __init__(self, attr_dict):
-    #     self.field_name = attr_dict.get("field", "")
-    #     self.props = attr_dict.get("props", "")
-
-
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+from os.path import splitext
+import uuid
+import xml.etree.ElementTree as ElTr
+import errno
 import ddl_classes
+from dbd_const import SQL_DBD_Init
+import sqlite3
+from os import remove
 
 __author__ = 'NovAtor'
-# from socket import gethostname
-# import xml.dom.minidom
-# import xml.dom
-import xml.etree.ElementTree as ET
-import sqlite3
-import xml.etree
 # from platform import system
 
 #
@@ -15,42 +16,53 @@ import xml.etree
 # else:
 #     dropPath = "D:/Dropbox/" if (gethostname() == "NovAtor-ПК") else "C:/Users/NovAtor/Dropbox/"
 # xmlPath = dropPath + "Programming/Python/20150221-093102/prjadm.xml"
-xmlTree = ET.parse("parseddl.xml")
+xml_path = "parsedbd.xml"
+xmlTree = ElTr.parse(xml_path)
 xmlRoot = xmlTree.getroot()
 
 
+# Next - debug function, but i still keep it
 def print_xml_tree(node, level=0):
     print("." * level, node.tag, ": ", node.attrib)
     for tagChild in node.findall("./*"):
         print_xml_tree(tagChild, level + 1)
 
 
-print(xmlRoot[0].tag)
-for chi1 in xmlRoot.findall("./*"):
-    print(chi1.tag)
-print(list(xmlRoot))
-print_xml_tree(xmlRoot)
-
-
-def parse_domains(root, domains):
+def parse_domains(root, domains, types=None):
     dom_num = 0
     # domains = []
-    for domain_dict in root.find("domains"):
+    for domain_el in root.find("domains"):
         dom_num += 1
         # print(" attr-s:", ["{}={}, ".format(k, v) for k, v in domain.attrib.items()])
-        domain = ddl_classes.Domain()
-
-        domain.name = domain_dict.get("name", "")
+        domain_obj = ddl_classes.Domain()
+        #
+        #       !!! Bellow use constraint.get("kind", ""), but type of constraint - not dict. It's Element (etree)
+        #       If want to use constraint["kind"] style, needed constraint.attrib["kind"] or changing loop with .attrib
+        #
+        domain_obj.name = domain_el.get("name")
         # TODO Использование .pop() изменит объект снаружи, однако это уменшит поиск, в теории. Нужно ли заменить?
-        domain.description = domain_dict.get("description", "")
-        domain.type = domain_dict.get("type", "")
-        domain.align = domain_dict.get("align", "")
-        domain.width = int(domain_dict.get("width", 0))
-        domain.precision = domain_dict.get("precision", 0)
-        domain.props = domain_dict.get("props", "")
-        domain.char_length = int(domain_dict.get("char_length", 0))
+        domain_obj.description = domain_el.get("description")
+        domain_obj.type = domain_el.get("type")
+        if types.__class__.__name__ == "dict":
+            if domain_obj.type is not None:
+                    types.setdefault(domain_obj.type)
+        domain_obj.align = domain_el.get("align", "L")
+        domain_obj.width = domain_el.get("width")
+        if domain_obj.width is not None:
+            domain_obj.width = int(domain_obj.width)
+        domain_obj.precision = domain_el.get("precision")
+        domain_obj.props = domain_el.get("props", "")
+        domain_obj.char_length = domain_el.get("char_length")
+        if domain_obj.char_length is not None:
+            domain_obj.char_length = int(domain_obj.char_length)
+        domain_obj.length = domain_el.get("length")
+        if domain_obj.length is not None:
+            domain_obj.length = int(domain_el.get("length"))
+        domain_obj.scale = domain_el.get("scale")
+        if domain_obj.scale is not None:
+            domain_obj.scale = int(domain_el.get("scale"))
 
-        domains.append(domain)
+        domains.append(domain_obj)
     return dom_num
 
 
@@ -64,11 +76,14 @@ def parse_tables(root, in_tables, domains):
         table_obj.description = table.get("description", "")
         table_obj.props = table.get("props", "")
         table_obj.ht_table_flags = table.get("ht_table_flags", "")
-        table_obj.fields = []
+        table_obj.temporal_mode = table.get("temporal_mode", None)
+        table_obj.means = table.get("means", None)
+        table_obj.fields = {}
         table_obj.pr_constraint = None
         table_obj.fr_constraints = []
+        table_obj.indexes = []
 
-        for field_dict in table:
+        for field_dict in table.findall("field"):
             field = ddl_classes.Field()
 
             field.name = field_dict.get("name", "")
@@ -88,44 +103,142 @@ def parse_tables(root, in_tables, domains):
 
 
 def parse_tables_other(root, in_tables):
+    i = 0
     for table in root.find("tables"):
-        for constraint in table.find("constraint"):
+        table_obj = in_tables[i]
+        for constraint in table.findall("constraint"):
+            items_field = table_obj.fields[constraint.get("items")]
             if constraint.get("kind", "") == "PRIMARY":
-                la = {}
+                table_obj.pr_constraint = ddl_classes.PrConstraint(items_field)
             else:
                 if constraint.get("kind", "") == "FOREIGN":
-                    lald = {}
-                # else:
-                #     print("Incorrect constraint")
+                    ref_table = next((t for t in in_tables if t.name == constraint.get("reference")), None)
+                    if ref_table is None:
+                        # !!!!!!!!!!!!!!!!!!!!!!!!!!!! ERROR HERE !!!!!!!!!!!!!!!
+                        table_obj.fr_constraints.append(ddl_classes.ForConstraint(items_field, None,
+                                                                                  constraint.get("props", "")))
+                    else:
+                        table_obj.fr_constraints.append(ddl_classes.ForConstraint(items_field, ref_table,
+                                                                                  constraint.get("props", "")))
+                        # else:
+                        #     print("Incorrect constraint")
+        for index in table.findall("index"):
+            table_obj.indexes.append(ddl_classes.Index(table_obj.fields[index.get("field")], index.get("props")))
+        # ++i
+        i += 1
+
 
 domain_lst = []
+# types_dict = {}
 domains_num = parse_domains(xmlRoot, domain_lst)
-tables = []
-tables_num = parse_tables(xmlRoot, tables, domain_lst)
-lala = []
-# parse_tables_other(xmlRoot, tables)
-# print(xmlRoot[0].tag, xmlRoot[0].attrib, xmlRoot[1].text.strip(), '3')
-# print(xmlRoot[1][0].tag, xmlRoot[1][0].attrib, xmlRoot[1][0].text, '3')
-# print(domain_lst)
-con = sqlite3.connect("temp.db")
+tables_lst = []
+tables_num = parse_tables(xmlRoot, tables_lst, domain_lst)
+parse_tables_other(xmlRoot, tables_lst)
+
+
+def silentrem(filename):
+    try:
+        remove(filename)
+    except OSError as e:  # this would be "except OSError, e:" before Python 2.6
+        if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
+            raise  # re-raise exception if a different error occurred
+
+db_path = splitext(xml_path)[0]+".db"
+silentrem(db_path)
+con = sqlite3.connect(db_path)
 cur = con.cursor()
-cur.executescript("""
-    create table person(
-        firstname,
-        lastname,
-        age
-    );
+cur.executescript(SQL_DBD_Init)
+print("Empty db-file was successfully created.")
+cur.execute("create temporary table domains_tmp (b,c,t_name,e,f,g,h,i,j,k,l,m,n,o,p);")
+for domain in domain_lst:
+    d_name = domain.name
+    d_description = domain.description
+    d_type_name = domain.type
+    d_length = domain.length
+    d_char_length = domain.char_length
+    d_precision = domain.precision
+    d_scale = domain.scale
+    d_width = domain.width
+    d_align = domain.align
+    d_show_null = ("show_null" in domain.props)
+    d_show_lead_nulls = ("show_lead_nulls" in domain.props)
+    d_thousands_separator = ("thousands_separator" in domain.props)
+    d_summable = ("summable" in domain.props)
+    d_case_sensitive = ("case_sensitive" in domain.props)
+    d_uuid = uuid.uuid4().hex
+    cur.execute("insert into domains_tmp values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);", (d_name, d_description,
+                d_type_name, d_length, d_char_length, d_precision, d_scale, d_width, d_align, d_show_null,
+                d_show_lead_nulls, d_thousands_separator, d_summable, d_case_sensitive, d_uuid))
+cur.executescript("""insert into dbd$domains select null, d.b, d.c, t.id, d.e, d.f, d.g,
+                  d.h, d.i, d.j, d.k, d.l, d.m, d.n, d.o, d.p
+                  from domains_tmp d inner join dbd$data_types t on d.t_name = t.type_id;
 
-    create table book(
-        title,
-        author,
-        published
-    );
+                  DROP TABLE domains_tmp;""")
+con.commit()
+#   Next not supported in sqlite3 (update... set.. FROM)
+# cur.execute("update dbd$domains "
+#             "set data_type_id = t.id "
+#             "from dbd$domains d "
+#             "inner join dbd$data_types t "
+#             "on d.type_name = t.id ")
+#   So...
+# cur.execute("""replace into dbd$domains
+#             (id, name, description, data_type_id, length, char_length,precision, scale, width, align, show_null,
+#             show_lead_nulls, thousands_separator, summable, case_sensitive, uuid, type_name)
+#             select d.id, d.name, d.description, t.id, d.length, d.char_length, d.precision,
+#             d.scale, d.width, d.align, d.show_null, d.show_lead_nulls, d.thousands_separator,
+#             d.summable boolean, d.case_sensitive, d.uuid, d.type_name
+#             from dbd$domains d inner join dbd$data_types t on d.type_name = t.id;""")
+# cur.execute("ALTER TABLE dbd$domains DROP COLUMN type_name;")
+con.commit()
+for table in tables_lst:
+    t_name = table.name
+    t_description = table.description
+    t_can_add = ("add" in table.props)
+    t_can_edit = ("edit" in table.props)
+    t_can_delete = ("delete" in table.props)
+    # t_r = ("r" in table.ht_table_flags)
+    t_temporal_mode = table.temporal_mode
+    t_means = table.means
+    t_uuid = uuid.uuid4().hex
+    cur.execute("insert into dbd$tables values (Null,Null,?,?,?,?,?,?,?,?);", (t_name, t_description,
+                t_can_add, t_can_edit, t_can_delete, t_temporal_mode, t_means, t_uuid,))
+con.commit()
+# cur.execute("create temporary constraints_tmp table fields_tmp (t_name,a,b,c,d,d_name,f,g,h,i,j,k,l,m);")
+# cur.execute("create temporary con_details_tmp table fields_tmp (t_name,a,b,c,d,d_name,f,g,h,i,j,k,l,m);")
+# cur.execute("create temporary indices table fields_tmp (t_name,a,b,c,d,d_name,f,g,h,i,j,k,l,m);")
+# cur.execute("create temporary ind_details_tmp table fields_tmp (t_name,a,b,c,d,d_name,f,g,h,i,j,k,l,m);")
+cur.execute("create table fields_tmp (t_name,a,b,c,d,d_name,g,h,i,j,k,l,m,n);")
+for table in tables_lst:
+    f_pos = 0
+    for fld_name, field in table.fields.items():
+        f_pos += 1
+        f_name = fld_name
+        f_rname = field.rname
+        f_description = field.description
+        f_d_name = field.domain_name
+        f_can_input = ("input" in field.props)
+        f_can_edit = ("edit" in field.props)
+        f_sh_in_grid = ("show_in_grid" in field.props)
+        f_sh_in_det = ("show_in_details" in field.props)
+        f_is_mean = ("is_mean" in field.props)
+        f_au_calc = ("autocalculated" in field.props)
+        f_required = ("required" in field.props)
+        f_uuid = uuid.uuid4().hex
+        cur.execute("insert into fields_tmp values (?,?,?,?,?,?,?,?,?,?,?,?,?,?);", (t_name, f_pos, f_name, f_rname,
+                    f_description, f_d_name, f_can_input, f_can_edit, f_sh_in_grid, f_sh_in_det, f_is_mean,
+                    f_au_calc, f_required, f_uuid))
+cur.executescript("""insert into dbd$fields select null, t.id, f.a, f.b, f.c,
+                  f.d, d.id, f.g, f.h, f.i, f.j, f.k, f.l, f.m, f.n
+                  from (fields_tmp f inner join dbd$tables t on f.t_name = t.name)
+                  inner join dbd$domains d on f.d_name = d.name;
 
-    insert into book(title, author, published)
-    values (
-        'Dirk Gently''s Holistic Detective Agency',
-        'Douglas Adams',
-        1987
-    );
-    """)
+                  DROP TABLE fields_tmp;""")
+con.commit()
+# TODO: Сделать разрешение проблемы id для constraint'ов и indexes'ов
+# for table in tables_lst:
+#     f_pos = 0
+#     for field in table.fields:
+
+print("Db-file update code executed successfully.")
+
